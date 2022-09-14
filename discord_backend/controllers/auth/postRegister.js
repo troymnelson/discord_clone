523a26336663bbd3d20d8d87795b0a4b1f8d4173
troy <troymnlsn@gmail.com>
2022-09-14 10:26:21 -0500

    initial

---
+++ discord_backend/controllers/auth/postRegister.js
@@ -0,0 +1,42 @@
+const User = require("../../models/User");
+const bcrypt = require('bcryptjs');
+
+const postRegister = async (req, res) => {
+    try {
+        const { username, mail, password } = req.body;
+
+        // check if user exists
+        const userExists = await User.exists({ mail: mail.toLowerCase() });
+
+        if (userExists) {
+            return res.status(409).send("E-mail already in use.")
+        }
+
+        // hash password
+
+        const encryptedPass = await bcrypt.hash(password, 10);
+
+        // create user document and save in database
+
+        const user = await User.create({
+            username,
+            mail: mail.toLowerCase(),
+            password: encryptedPass
+        });
+
+        // create JWT
+        const token = 'JWT token';
+
+        res.status(201).json({
+            userDetails: {
+                mail: user.mail,
+                token: token,
+                username: user.username
+            }
+        })
+    } catch (err) {
+        return res.status(500).send('Error occurred. Please try again.');
+    }
+};
+
+module.exports = postRegister;
