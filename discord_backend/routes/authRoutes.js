ef0df607724b4b57005a9c92896cde8defb50de6
troy <troymnlsn@gmail.com>
2022-09-17 22:48:29 -0500

    added frontend with react redux and installed dependencies

--- discord_backend/routes/authRoutes.js
+++ discord_backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authControllers = require('../controllers/auth/authControllers');
 const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator({});
+const auth = require('../middleware/auth');
 
 const registerSchema = Joi.object({
     username: Joi.string().min(3).max(12).required(),
@@ -18,5 +19,9 @@ const loginSchema =  Joi.object({
 router.post('/register', validator.body(registerSchema), authControllers.controllers.postRegister);
 
 router.post('/login', validator.body(loginSchema), authControllers.controllers.postLogin);
+
+router.get('/test', auth, (req, res) => {
+    res.send('working');
+});
 
 module.exports = router;
